a4fce75678493695547ef10b99a37b86fe2c0735
Sultana Jahan Tahmina <hscsultana22@gmail.com>
2025-11-09 19:34:51 +0600

    Update Game.tsx

--- Game.tsx
+++ Game.tsx
@@ -1,1 +1,14 @@
 #DesignAMemoryGame
+
+  import React, {useState, useEffect} from 'react';
+import'./App.css';
+
+interface Card {
+  id: number;
+  value: string;
+  isFlipped: boolean;
+  isMatched: boolean;
+}
+
+const generateCards = (): Card[] => {
+  const values = ['🐶', '😺', ' 
